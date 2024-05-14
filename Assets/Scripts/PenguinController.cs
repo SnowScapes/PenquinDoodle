c8eaea9b0c87892cbfc44e7c96dcf5f2e97c16f0
SnowScapes <siwon2113@gmail.com>
2024-05-14 10:43:59 +0900

    Update PenguinController.cs

--- Assets/Scripts/PenguinController.cs
+++ Assets/Scripts/PenguinController.cs
@@ -52,9 +52,5 @@ public class PenguinController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.collider.gameObject.CompareTag("Finish"))
-        {
-            
-        }
     }
 }
