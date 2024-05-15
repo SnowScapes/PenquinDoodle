0dc2040f690bb354b67ee0755792c30837375f6a
SnowScapes <siwon2113@gmail.com>
2024-05-16 00:01:25 +0900

    improve Time update logic

--- Assets/Scripts/TimeText.cs
+++ Assets/Scripts/TimeText.cs
@@ -10,6 +10,7 @@ public class TimeText : MonoBehaviour
     [SerializeField] private Text timeText;
 
     private string currentTime;
+    private bool firstInitialized;
 
     private void Awake()
     {
@@ -18,20 +19,26 @@ public class TimeText : MonoBehaviour
 
     private void Start()
     {
+        firstInitialized = true;
         StartCoroutine(updateTimeText());
     }
     private IEnumerator updateTimeText()
     {
-        // yeild return new WaitForSeconds(1.0f)의 경우
+        // yeild return new WaitForSeconds()의 경우
         // 매번 새로운 waitforseconds 인스턴스를 만들기 때문에
         // new를 한번만 할 수 있도록 만들었다.
-        WaitForSeconds delay1sec = new WaitForSeconds(1.0f);
-
+        WaitForSeconds delay1min = new WaitForSeconds(60f);
+        
         while (true)
         {
             currentTime = DateTime.Now.ToString("hh:mm");
             timeText.text = currentTime;
-            yield return delay1sec;
+            
+            //첫 실행때는 초를 계산하여 다음 시계의 업데이트 시간을 결정한다.
+            if (firstInitialized)
+                yield return new WaitForSeconds(60 - DateTime.Now.Second);
+            else
+                yield return delay1min;
         }
     }
 }
